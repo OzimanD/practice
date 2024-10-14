bb4879d71066917e3e0cc93fd378ff5759c33f5c
alinatelychko <adelin9999@gmail.com>
2024-10-14 19:59:28 +0200

    1 task

--- src/ALina/main.java
+++ src/ALina/main.java
@@ -1,4 +1,27 @@
 package ALina;
 
 public class main {
+    public static void main(String[] args) {
+        System.out.println( Day(2));
+
+    }
+
+    static String Day (int a){
+        String result;
+
+        switch (a){
+            case 1:
+                result = "Monday";
+                break;
+            case 2:
+                result = "Tuesday";
+                break;
+            case 3:
+                result = "Wednesday";
+                break;
+            default:
+                result = " There is no such a day ";
+        }
+        return result;
+    }
 }
