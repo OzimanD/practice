afa047fe50a1c2b17fad39a72f038cf8b1e3cda4
kotevaska <kotevasbka@gmail.com>
2025-03-01 20:42:31 +0200

    Serega pravil myFirstClassOnGIT

--- src/Sergey/myFirstClassOnGIT.java
+++ src/Sergey/myFirstClassOnGIT.java
@@ -1,16 +1,15 @@
 package Sergey;
 
 public class myFirstClassOnGIT {
-        public static void main(String[] args) {
-    res_oper(2,4);
+    public static void main(String[] args) {
+        res_oper(2,4);
+    }
+    static void res_oper(int i_a, int i_b){
+        int i_tmp = 0;
+        for (int i = i_a; i <= i_b; i++) {
+            System.out.println(i);
+            ++i_tmp;
         }
-
-        static void res_oper(int i_a, int i_b){
-            int i_tmp = 0;
-            for (int i = i_a; i <= i_b; i++) {
-                System.out.println(i);
-                ++i_tmp;
-            }
-            System.out.println(i_tmp);
-        }
+        System.out.println(i_tmp);
+    }
 }
